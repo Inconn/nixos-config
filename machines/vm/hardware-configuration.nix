86af1e1dfa96fc02621f35a5271a29256f1fea2a
Inconn <62788103+Inconn@users.noreply.github.com>
2024-03-21 14:04:04 -0500

    change to new vm

--- machines/vm/hardware-configuration.nix
+++ machines/vm/hardware-configuration.nix
@@ -5,9 +5,9 @@
 		(modulesPath + "/profiles/qemu-guest.nix")
 	];
 
-	boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
+	boot.initrd.availableKernelModules = [ "virtio_pci" "ahci" "sr_mod" "virtio_blk" ];
 	boot.initrd.kernelModules = [ ];
-	boot.kernelModules = [ "kvm-intel" ];
+	boot.kernelModules = [ ];
 	boot.extraModulePackages = [ ];
 
 	networking.useDHCP = lib.mkDefault true;
