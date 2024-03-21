da77e20be6fa4742b5e5787b5dbc344e1ce18e7f
Inconn <62788103+Inconn@users.noreply.github.com>
2024-03-21 15:11:00 -0500

    make user normal user

--- system/default.nix
+++ system/default.nix
@@ -13,6 +13,7 @@
 
 	users.mutableUsers = false;
 	users.users.incon = {
+		isNormalUser = true;
 		extraGroups = [ "wheel" ];
 		initialHashedPassword = "$6$AG9VjEF8n96p4keh$Q8UWH1czgs.mH6PPK.Xk3KJ2jivRryG770KN6E0cEhfVtlmDLEbimavbdbzR7lK.wo29EbFnvJmQ1YifH6TvJ0";
 	};
